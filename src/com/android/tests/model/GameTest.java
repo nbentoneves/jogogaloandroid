606f5e83dbf1b0d56327359702a9c7dbb424e7c7
Nuno Bento <nbento.neves@gmail.com>
2013-09-03 14:13:26 +0100

    Start Tests Model
    Signed-off-by: Nuno Bento <nbento.neves@gmail.com>

---
+++ src/com/android/tests/model/GameTest.java
@@ -0,0 +1,65 @@
+package com.android.tests.model;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import android.test.AndroidTestCase;
+
+public class GameTest extends AndroidTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlaying() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckVictory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckFirstDiagonal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckSecoundDiagonal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckEmpate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckVictoryVertical() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckVictoryHorizontal() {
+		fail("Not yet implemented");
+	}
+
+}
