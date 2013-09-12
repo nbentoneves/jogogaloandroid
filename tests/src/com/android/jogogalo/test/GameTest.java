d7b1240bb076532a6d12b769c1ab64ae952b08a2
nbento <nbento.neves@gmail.com>
2013-09-12 12:42:09 +0100

    Auto Generator Code Android
    Signed-off-by: nbento <nbento.neves@gmail.com>

--- tests/src/com/android/jogogalo/test/GameTest.java
+++ tests/src/com/android/jogogalo/test/GameTest.java
@@ -1,8 +1,8 @@
 package com.android.jogogalo.test;
 
-import com.android.model.Computer;
-import com.android.model.Game;
-import com.android.model.Human;
+import com.jogogalo.model.Computer;
+import com.jogogalo.model.Game;
+import com.jogogalo.model.Human;
 
 import android.test.AndroidTestCase;
 
@@ -11,11 +11,18 @@ public class GameTest extends AndroidTestCase {
 	private Computer computer = new Computer(0, 0, 'X');
 	private Human human = new Human(0, 'O', "Laura");
 	private Game game;
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		// TODO Auto-generated method stub
 		game = new Game(computer, human);
+
+		Computer computer = new Computer(0, 0, 'X');
+		Human human = new Human(0, 'O', "Laura");
+
+		game.setPlayer1(computer);
+		game.setPlayer2(human);
+
 		super.setUp();
 	}
 
@@ -25,12 +32,12 @@ public class GameTest extends AndroidTestCase {
 		game = null;
 		super.tearDown();
 	}
-	
+
 	/**
 	 * Check if human victory horizontal
 	 */
 	public void testCheckVictoryHorizontalHuman() {
-		
+
 		game.playing(computer, 0);
 		game.playing(human, 3);
 		game.playing(computer, 1);
@@ -43,7 +50,201 @@ public class GameTest extends AndroidTestCase {
 		int result = game.checkVictoryHorizontal();
 
 		assertEquals(expect_result, result);
-		
+
 	}
-	
+
+	/**
+	 * Check if computer victory horizontal
+	 */
+	public void testCheckVictoryHorizontalComputer() {
+
+		game.playing(computer, 0);
+		game.playing(human, 3);
+		game.playing(computer, 1);
+		game.playing(human, 4);
+		game.playing(computer, 2);
+
+		int expect_result = 2;
+
+		int result = game.checkVictoryHorizontal();
+
+		assertEquals(expect_result, result);
+
+	}
+
+	/**
+	 * Check draw horizontal
+	 */
+	public void testCheckVictoryHorizontalDraw() {
+
+		game.playing(computer, 0);
+		game.playing(human, 3);
+		game.playing(computer, 1);
+		game.playing(human, 4);
+
+		int expect_result = -1;
+
+		int result = game.checkVictoryHorizontal();
+
+		assertEquals(expect_result, result);
+
+	}
+
+	/**
+	 * Check if human victory vertical
+	 */
+	public void testCheckVictoryVerticalHuman() {
+
+		game.playing(computer, 1);
+		game.playing(human, 0);
+		game.playing(computer, 2);
+		game.playing(human, 3);
+		game.playing(computer, 8);
+		game.playing(human, 6);
+
+		int expect_result = 1;
+
+		int result = game.checkVictoryVertical();
+
+		assertEquals(expect_result, result);
+
+	}
+
+	/**
+	 * Check if computer victory vertical
+	 */
+	public void testCheckVictoryVerticalComputer() {
+
+		game.playing(computer, 1);
+		game.playing(human, 0);
+		game.playing(computer, 4);
+		game.playing(human, 3);
+		game.playing(computer, 7);
+
+		int expect_result = 2;
+
+		int result = game.checkVictoryVertical();
+
+		assertEquals(expect_result, result);
+
+	}
+
+	/**
+	 * Check draw vertical
+	 */
+	public void testCheckVictoryVerticalDraw() {
+
+		game.playing(computer, 1);
+		game.playing(human, 0);
+		game.playing(computer, 4);
+
+		int expect_result = -1;
+
+		int result = game.checkVictoryVertical();
+
+		assertEquals(expect_result, result);
+
+	}
+
+	/**
+	 * Check if human victory first diagonal
+	 */
+	public void testCheckVictoryFirstDiagonalHuman() {
+
+		game.playing(computer, 1);
+		game.playing(human, 0);
+		game.playing(computer, 3);
+		game.playing(human, 4);
+		game.playing(computer, 2);
+		game.playing(human, 8);
+
+		int expect_result = 1;
+
+		int result = game.checkFirstDiagonal();
+
+		assertEquals(expect_result, result);
+
+	}
+
+	/**
+	 * Check if computer victory first diagonal
+	 */
+	public void testCheckVictoryFirstDiagonalComputer() {
+
+		game.playing(computer, 0);
+		game.playing(human, 1);
+		game.playing(computer, 4);
+		game.playing(human, 3);
+		game.playing(computer, 8);
+
+		int expect_result = 2;
+
+		int result = game.checkFirstDiagonal();
+
+		assertEquals(expect_result, result);
+
+	}
+
+	/**
+	 * Check if human victory second diagonal
+	 */
+	public void testCheckVictorySecondDiagonalHuman() {
+
+		game.playing(computer, 1);
+		game.playing(human, 2);
+		game.playing(computer, 5);
+		game.playing(human, 4);
+		game.playing(computer, 8);
+		game.playing(human, 6);
+
+		int expect_result = 1;
+
+		int result = game.checkSecondDiagonal();
+
+		assertEquals(expect_result, result);
+
+	}
+
+	/**
+	 * Check if computer victory second diagonal
+	 */
+	public void testCheckVictorySecondDiagonalComputer() {
+
+		game.playing(computer, 2);
+		game.playing(human, 1);
+		game.playing(computer, 4);
+		game.playing(human, 5);
+		game.playing(computer, 6);
+
+		int expect_result = 2;
+
+		int result = game.checkSecondDiagonal();
+
+		assertEquals(expect_result, result);
+
+	}
+
+	/**
+	 * Check draw
+	 */
+	public void testCheckDraw() {
+
+		game.playing(computer, 1);
+		game.playing(human, 0);
+		game.playing(computer, 3);
+		game.playing(human, 2);
+		game.playing(computer, 4);
+		game.playing(human, 5);
+		game.playing(computer, 8);
+		game.playing(human, 6);
+		game.playing(human, 7);
+
+		int expect_result = 0;
+
+		int result = game.checkDraw();
+
+		assertEquals(expect_result, result);
+
+	}
+
 }
